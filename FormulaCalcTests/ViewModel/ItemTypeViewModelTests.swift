f987e20351f83af20b229b1e92ee3fb6503c2ece
Hironori Ichimiya <hiron@hironytic.com>
2017-04-02 11:22:57 +0900

    Add test cases

--- FormulaCalcTests/ViewModel/ItemTypeViewModelTests.swift
+++ FormulaCalcTests/ViewModel/ItemTypeViewModelTests.swift
@@ -124,5 +124,115 @@ class ItemTypeViewModelTests: XCTestCase {
         
         waitForExpectations(timeout: 3.0)
     }
+
+    func testOptionsAccessoryType() {
+        // SCENARIO:
+        // (1) Check the each option's accessory type.
+        
+        let viewModel = ItemTypeViewModel(locator: mockLocator, id: "sheet_item_zero")
+        
+        var listDisposeBag: DisposeBag!
+        let exp = expectation(description: "String is only checked")
+        viewModel.typeList.subscribe (onNext: { list in
+            listDisposeBag = DisposeBag()
+            
+            var conditions = [false, false, false]
+            func fulfillCondition(index: Int) {
+                conditions[index] = true
+                if !conditions.contains(false) {
+                    exp.fulfill()
+                }
+            }
+            
+            if list.count == 3 {
+                list[0].accessoryType.subscribe(onNext: { (type: UITableViewCellAccessoryType) in
+                    if type == .none {
+                        fulfillCondition(index: 0)
+                    }
+                })
+                .disposed(by: listDisposeBag)
+                
+                list[1].accessoryType.subscribe(onNext: { (type: UITableViewCellAccessoryType) in
+                    if type == .checkmark {
+                        fulfillCondition(index: 1)
+                    }
+                })
+                .disposed(by: listDisposeBag)
+                
+                list[2].accessoryType.subscribe(onNext: { (type: UITableViewCellAccessoryType) in
+                    if type == .none {
+                        fulfillCondition(index: 2)
+                    }
+                })
+                .disposed(by: listDisposeBag)
+            }
+        }, onDisposed: {
+            listDisposeBag = nil
+        })
+        .disposed(by: disposeBag)
+        
+        waitForExpectations(timeout: 3.0)
+    }
     
+    func testSelectItemType() {
+        // SCENARIO:
+        // (1) "String" is checked
+        // (2) User taps "Numeric"
+        // (3) The type is updated to .string by SheetItemStore
+        // (4) It is refrected to UI
+
+        let viewModel = ItemTypeViewModel(locator: mockLocator, id: "sheet_item_zero")
+        
+        let accessoryTypes = viewModel.typeList
+            .flatMapLatest { (list) -> Observable<(UITableViewCellAccessoryType, UITableViewCellAccessoryType, UITableViewCellAccessoryType)> in
+                return Observable
+                    .combineLatest(list[0].accessoryType, list[1].accessoryType, list[2].accessoryType, resultSelector: { ($0, $1, $2) })
+            }
+        
+        // (1) "String" is checked
+        let accessoryTypeObserver = FulfillObserver(expectation(description: "String is checked")) { (type0: UITableViewCellAccessoryType, type1: UITableViewCellAccessoryType, type2: UITableViewCellAccessoryType) in
+            return type0 == .none
+                && type1 == .checkmark
+                && type2 == .none
+        }
+        accessoryTypes
+            .bindTo(accessoryTypeObserver)
+            .disposed(by: disposeBag)
+
+        waitForExpectations(timeout: 3.0)
+
+        let expectUpdateType = expectation(description: "OnUpdateType is fired")
+        let sheetItemStore0 = mockLocator.resolveSheetItemStore(id: "sheet_item_zero") as! MockSheetItemStore
+        var updateCount = 0
+        sheetItemStore0.updateTypeHandler = { type in
+            updateCount += 1
+            
+            // (3) The type is updated to .string by SheetItemStore
+            if type == .numeric {
+                expectUpdateType.fulfill()
+            }
+            if let sheetItem = sheetItemStore0.item {
+                sheetItem.type = type
+                sheetItemStore0.update(item: sheetItem)
+            }
+        }
+        
+        // (4) It is refrected to UI
+        accessoryTypeObserver.reset(expectation(description: "Nueric is checked")) { (type0: UITableViewCellAccessoryType, type1: UITableViewCellAccessoryType, type2: UITableViewCellAccessoryType) in
+            return type0 == .checkmark
+                && type1 == .none
+                && type2 == .none
+            
+        }
+        
+        // (2) User taps "Numeric"
+        viewModel.typeList
+            .subscribe (onNext: { list in
+                viewModel.onSelect.onNext(list[0])
+            })
+            .disposed(by: disposeBag)
+
+        waitForExpectations(timeout: 3.0)
+        XCTAssertEqual(updateCount, 1)
+    }
 }
